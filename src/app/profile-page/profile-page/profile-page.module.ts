55037cc939f5b8e71d3dd34d1cf14005b7cecb6a
Аскар Хабибуллин <a.khabibullin@innopolis.ru>
2023-05-26 14:38:23 +0300

    добавил девопс штучки, наверстал основную страницу в неоморфном виде

--- src/app/profile-page/profile-page/profile-page.module.ts
+++ src/app/profile-page/profile-page/profile-page.module.ts
@@ -1,14 +1,31 @@
+import { GTMTemplate } from './../shared/directives/template-directive';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfilePageComponent } from '../profile-page.component';
 import { ProfileRoutingModule } from './profile-routing.module';
+import { ChatProfileComponent } from '../chat-profile/chat-profile.component';
+import {
+  CardSectionsComponent,
+} from '../card-sections/card-sections.component';
+import { CardComponent } from '../card/card.component';
+import { ProfileHeaderComponent } from '../profile-header/profile-header.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  declarations: [ProfilePageComponent],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule
+  declarations: [
+    ProfilePageComponent,
+    ChatProfileComponent,
+    CardSectionsComponent,
+    CardComponent,
+    GTMTemplate,
+    ProfileHeaderComponent,
   ],
-  exports: [ProfilePageComponent]
+  imports: [CommonModule, ProfileRoutingModule, ButtonModule],
+  exports: [
+    ProfilePageComponent,
+    ChatProfileComponent,
+    CardSectionsComponent,
+    GTMTemplate,
+  ],
 })
-export class ProfilePageModule { }
+export class ProfilePageModule {}
