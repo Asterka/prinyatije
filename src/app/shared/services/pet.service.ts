229247e5393575c4b91869dd9afd4edaeddbdcaa
skymarce <duraev-andrey2014@yandex.ru>
2023-05-27 16:08:39 +0300

    Реализовано создание питомца

---
+++ src/app/shared/services/pet.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PageDtoPetDto, PetCreateUpdateDto, PetDto, PetsControllerService } from 'src/app/openapi';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PetService {
+
+  constructor(
+    private petService: PetsControllerService
+  ) { }
+
+  createPet(pet: PetCreateUpdateDto) {
+    this.petService.create1(pet).subscribe();
+  }
+
+  getPets(): Observable<PageDtoPetDto> {
+    return this.petService.findByUserId();
+  }
+}
